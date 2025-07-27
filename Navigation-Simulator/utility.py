207ac51611eb20d24d93e182ed4f5ee959dd529c
Subhaan Ahmed <subhaan.ahmed-23@outlook.com>
2025-07-27 10:44:19 +0100

    test commit 2

--- Navigation-Simulator/utility.py
+++ Navigation-Simulator/utility.py
@@ -3,9 +3,6 @@ import goal
 import robot
 import path_planning_base
 
-
-obstacle_colour = (10, 10, 10)
-
 def drawGrid(screen, COLOUR, blocksize=20, window_size=(500, 500)):
     for x in range(0, window_size[0], blocksize):
         for y in range(0, window_size[0], blocksize):
@@ -15,13 +12,12 @@ def drawGrid(screen, COLOUR, blocksize=20, window_size=(500, 500)):
 def drawScene(screen, grid_colour, block_size, robot: robot.Robot, goal_point: goal.Goal, WINDOW_SIZE):
     
     drawGrid(screen, grid_colour, block_size, WINDOW_SIZE)
-    
     robot.drawRobot(screen)
     goal_point.drawGoal(screen)
     
 def drawObstacles(screen, obstacle_coords, block_size):
     for x, y in obstacle_coords:
-        pg.draw.rect(screen, (0,0,0), (x, y, block_size, block_size))
+        pg.draw.rect(screen, (0,0,0), (x, y, block_size, block_size)) #make obstacles black
     
 background_colour = (255, 255, 255) 
 grid_colour = (0, 0, 0)
